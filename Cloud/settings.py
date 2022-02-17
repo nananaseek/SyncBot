5acf5fa66158f8f542847ff4e9bb7ec5cd1304df
HobMartin <ivanderda1999@gmail.com>
2022-02-18 00:36:43 +0200

    add database env

--- Cloud/settings.py
+++ Cloud/settings.py
@@ -181,5 +181,5 @@ SIMPLE_JWT = {
 
 import dj_database_url
 
-prod_db = dj_database_url.config(conn_max_age=500)
+prod_db = dj_database_url.config(conn_max_age=500, ssl_require=True)
 DATABASES["default"].update(prod_db)
